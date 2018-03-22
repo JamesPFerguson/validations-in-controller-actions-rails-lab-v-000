15e7a8fcedac71af993f98d34e2a73b9581435b6
James Ferguson <ncjamesferguson@gmail.com>
2018-03-22 18:29:08 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,9 +25,14 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+    post = Post.new(post_params)
 
-    redirect_to post_path(@post)
+    if post.valid?
+      Post.find(params[:id]).update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
